112cf0d9c79572db050b8e17873ede6bcf048e18
Mahesh Gaikwad <91421427+777ahesh@users.noreply.github.com>
2022-04-17 09:09:22 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import Topbar from "./components/topbar/Topbar";
-import "./App.css";
 import Home from "./components/pages/Home";
 import Welcome from "./components/welcome/Welcome";
 import Installation from "./components/installation/Installation";
 import Order from "./components/order/Order";
 import Final from "./components/final/Final";
+import "./App.css";
 function App() {
   return (
     <div>
